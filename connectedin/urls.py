b45d512dba14e186845fbff89a72fa729678f4c4
Tatiana <tatianamariassousa@gmail.com>
2020-09-25 03:19:27 -0300

    atualização do projeto connectedin

--- connectedin/urls.py
+++ connectedin/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from perfis import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = 'index'),
+    path('perfil/<int:perfil_id>', views.exibir, name = 'exibir'),
+    path('perfis/<int:perfil_id>/convidar', views.convidar, name = 'convidar'),
 ]
