2d857d8d4c4f213b4e4f29f4bffb7ba34771b24f
Tatiana <tatianamariassousa@gmail.com>
2020-10-13 03:10:53 -0300

    Atualização 13/10/2020

--- connectedin/urls.py
+++ connectedin/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('perfil/<int:perfil_id>', views.exibir, name = 'exibir'),
     path('perfis/<int:perfil_id>/convidar', views.convidar, name = 'convidar'),
+    path('convite/<int:convite_id>/aceitar', views.aceitar, name='aceitar'),
 ]
--- perfis/models.py
+++ perfis/models.py
@@ -5,6 +5,7 @@ class Perfil(models.Model):
     email = models.CharField(max_length=255, null=False)
     telefone = models.CharField(max_length=15, null=False)
     nome_empresa = models.CharField(max_length=255, null=False)
+    contatos = models.ManyToManyField('self')
 
     def convidar(self, perfil_convidado):
         convite = Convite(solicitante=self, convidado=perfil_convidado)
@@ -13,3 +14,8 @@ class Perfil(models.Model):
 class Convite(models.Model):
     solicitante = models.ForeignKey(Perfil, related_name='convites_feitos', on_delete = models.CASCADE)
     convidado = models.ForeignKey(Perfil, related_name='convites_recebidos', on_delete = models.CASCADE)
+
+    def aceitar(self):
+        self.convidado.contatos.add(self.solicitante)
+        self.solicitante.contatos.add(self.convidado)
+        self.delete()
